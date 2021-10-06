fd75908fdbbd2ae1710eb9608649b330b2b4f069
lordwolf81 <lordwolf81@gmail.com>
2021-10-06 12:41:31 +0300

    состояние компонента
    useState()

--- src/App.js
+++ src/App.js
@@ -1,17 +1,42 @@
-import React from "react";
+import React, { useState } from "react";
 //react импортируем в каждый файл где мы создаем компонент
+//
+/*
+react не понимает что в конкретном компоненте произошли какие-то изменения,
+ему это необходимо явно сообщить .
+для этого придумано состоянии(State). каждый компонент обладает каким-то состоянием и при изменении этого состояния react понимает что произошли изменения и перерисовывает компонент
+ */
 function App() {
-  let likes = 0;
+  const [likes, setLikes] = useState(0);
+  console.log(likes);
+  console.log(setLikes);
+  function increment() {
+    setLikes(likes + 1);
+  }
+  function decrement() {
+    setLikes(likes - 1);
+  }
   return (
     <div className="App">
       <h1>{likes}</h1>
-      <button onClick={() => (likes += 1)}> Increment</button>
-      <button onClick={() => (likes -= 1)}> Decrement </button>
+      <button onClick={increment}> Increment</button>
+      <button onClick={decrement}> Decrement </button>
     </div>
   );
 }
 /*
-{} для связывания в шаблоне
 
+const state = useState();
+console.log(state);
+
+внутри -  это массив состоящий из двух элементов
+первый элемент это само значение которое мы объявили
+второй элемент функция, предназначеная для того чтобы изменять
+
+поскольку мы знаем что estate возвращает массив из двух объектов
+мы можем сделать деструктуризацию и сразу получить само состояние и функцию
+
+и сразу инициализируем , н-р 5
+const [state, setState ]= useState(5);
 */
 export default App;
