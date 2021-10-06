abab627b3a11374ac5640e39fa7aad7212da0399
lordwolf81 <lordwolf81@gmail.com>
2021-10-06 12:09:10 +0300

    связь переменной, события, шаблона
    {} синтаксис
    нет отрисовки измемений VDom

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React from "react";
+//react импортируем в каждый файл где мы создаем компонент
 function App() {
+  let likes = 0;
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>{likes}</h1>
+      <button onClick={() => (likes += 1)}> Increment</button>
+      <button onClick={() => (likes -= 1)}> Decrement </button>
     </div>
   );
 }
+/*
+{} для связывания в шаблоне
 
+*/
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,30 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+//импортируем ядро реактора
+import ReactDOM from "react-dom";
+// импортируем react дом для того чтобы монтировать наш компонент в приложении
 
+import App from "./App";
+ReactDOM.render(<App />, document.getElementById("root"));
+/* первым параметром мы передаем компонент,который необходимо отрисовать
+ вторым блок, в который мы этот компонент будем монтировать  */
+
+/*
+ ReactDOM.render( <div> Text </div> , document.getElementById("root"));
+ мы можем здесь создать блок div и его что-нибудь поместить например какой то
+текст эта структура может быть сколь угодно сложно и если сейчас мы откроем
+приложение то увидим эту надпись
+
+
+kаким образом мы пишем html-разметку внутри js файла?
+такой синтаксис называется JSX, он является расширением java script и упрощает написание кода
+ReactDOM.render(<div> <button>Text</button> </div>)
+то же самое без него
+ReactDOM.render(React.createElement('div',{}, React.createElement('button',{}, 'Кнопка')));
+React.createElement('type', "props", "children")
+*/
+
+/*
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+  React.createElement("button",
+  { onClick: () => console.log("Кнопка"), disabled: false }, "Кнопка"), document.getElementById("root"));
+ */
