b9b1f8c3f8e14f8b13fa5054b516e6a47808c887
lordwolf81 <lordwolf81@gmail.com>
2021-10-07 14:01:08 +0300

    вывод массивов данных, inline-стили

--- src/App.js
+++ src/App.js
@@ -1,30 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 import PostItem from "./components/PostItem";
 function App() {
+  const [posts, setPosts] = useState([
+    { id: 1, title: "JavaScript", body: "description" },
+    { id: 2, title: "JavaScript2", body: "description2" },
+    { id: 3, title: "JavaScript3", body: "description3" },
+  ]);
   return (
     <div className="App">
-      <PostItem post={{ id: 1, title: "JavaScript", body: "description" }} />
+      <h1 style={{ textAlign: "center" }}>Список постов</h1>
+      {posts.map((post) => (
+        <PostItem post={post} key={post.id} />
+      ))}
     </div>
   );
 }
-/* передача данных */
+
+/* вывод массивов данных */
 /*
-<PostItem value={"test"} item={{ title: 0 }}
->>const PostItem = (props) => {...}
-    {value: 'test', item: {…}, number: 42}
-      item: {title: 0}
-      number: 42
-      value: "test"
-    [[Prototype]]: Object
-
-    {props.number }
-    {props.item.title}
-
+key={post.id}
+- ключ должен хранить какое-то уникальное значение
+- должен быть статичным
+- !!!не использовать индекс элемента в массиве, (чтобы не вызать лишних перерисовок)
 */
 
 /* добавление стилей */
 /*
+- inline-стили
+ <h1 style={{ textAlign: "center" }}>
+
 - className вместо class
 */
 export default App;
