5cdae0f8601fc76cdfabdf19c3cee0547acc5d1b
Yaroslav Teslenko <teslenko.yaroslav21@gmail.com>
2021-10-24 18:57:43 +0300

    вывод коммента-в к посту

--- src/pages/PostIdPage.jsx
+++ src/pages/PostIdPage.jsx
@@ -3,9 +3,6 @@ import { useParams } from "react-router-dom";
 import PostService from "../API/PostService";
 import Loader from "../components/UI/Loader/Loader";
 import { useFetching } from "../hooks/useFetching";
-// import { useFetching } from "../hooks/useFetching";
-// import PostService from "../API/PostService";
-// import Loader from "../components/UI/Loader/Loader";
 
 const PostIdPage = () => {
   const params = useParams(); /* объект с парам-ми маршрута */
@@ -14,9 +11,18 @@ const PostIdPage = () => {
     const response = await PostService.getById(id);
     setPost(response.data);
   });
+
+  const [comments, setComments] = useState([]);
+  const [fetchComments, isComLoading, comError] = useFetching(async (id) => {
+    const response = await PostService.getCommentsByPostId(id);
+    setComments(response.data);
+  });
+
   useEffect(() => {
     fetchPostById(params.id);
+    fetchComments(params.id);
   }, []);
+
   return (
     <div>
       <h1>Вы открыли страницу поста c ID = {params.id}</h1>
@@ -25,6 +31,19 @@ const PostIdPage = () => {
       ) : (
         <div>
           {post.id}. {post.title}
+        </div>
+      )}
+      <h1>Комментарии</h1>
+      {isComLoading ? (
+        <Loader />
+      ) : (
+        <div>
+          {comments.map((comm) => (
+            <div key={comm.id} style={{ marginTop: 15 }}>
+              <h5>{comm.email}</h5>
+              <div>{comm.body}</div>
+            </div>
+          ))}
         </div>
       )}
     </div>
