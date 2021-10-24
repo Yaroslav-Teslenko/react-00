c074d0522a1b1a947fd1e429b588364d894c823c
Yaroslav Teslenko <teslenko.yaroslav21@gmail.com>
2021-10-24 17:42:18 +0300

    группировкя маршрутов, перенаправление

--- src/App.js
+++ src/App.js
@@ -1,27 +1,33 @@
 import React from "react";
 import "./styles/App.css";
-import { BrowserRouter, Route } from "react-router-dom";
+import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
 import About from "./pages/About";
 import Posts from "./pages/Posts";
+import Error from "./pages/Error";
+import Navbar from "./components/UI/Navbar/Navbar";
 function App() {
   return (
     /* чтобы реализовать роутинг нам необходимо все приложение обернуть в компонент BrowserRouter.
     именно он будет отслеживать изменение пути и перерисовывать компоненты */
     <BrowserRouter>
-      <div className="navbar">
-        <div className="navbar__links">
-          {/* при нажатии на эти ссылки у нас происходит обновление страницы что уже нарушает
-          принцип single page applications поскольку все переходы должны быть без перезагрузки  */}
-          <a href="/about">O prog</a>
-          <a href="/posts">Pages </a>
-        </div>
-      </div>
-      <Route path="/about">
-        <About />
-      </Route>
-      <Route path="/posts">
-        <Posts />
-      </Route>
+      <Navbar />
+      {/* switch позволяет группировать маршруты и выбрать хотя бы один из тех который есть внутри.
+      если не один из маршрутов не отработал мы можем сделать какой-то общий случай,
+      например выводить страницу с ошибкой или же переводить на другую страницу
+        <Redirect to="/error" />
+      чтобы переводить на этот маршрут нам необходимо его объявить  <Route path="/error">*/}
+      <Switch>
+        <Route path="/about">
+          <About />
+        </Route>
+        <Route path="/posts">
+          <Posts />
+        </Route>
+        <Route path="/error">
+          <Error />
+        </Route>
+        <Redirect to="/error" />
+      </Switch>
     </BrowserRouter>
   );
 }
