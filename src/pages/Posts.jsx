d59b74f94872f8accadc2d562cad6b827f33917b
Yaroslav Teslenko <teslenko.yaroslav21@gmail.com>
2021-10-25 13:44:42 +0300

    add select для кол-ва постов на странице. FINAL

--- src/pages/Posts.jsx
+++ src/pages/Posts.jsx
@@ -14,6 +14,7 @@ import PostService from "../API/PostService";
 import { useFetching } from "../hooks/useFetching";
 import { getPageCount, getPagesArray } from "../utils/pages";
 import Pagination from "../components/UI/pagination/Pagination";
+import { useObserver } from "../hooks/useObserver";
 
 function Posts() {
   const [posts, setPosts] = useState([]);
@@ -37,34 +38,13 @@ function Posts() {
   let pageArray = getPagesArray(totalPages);
   const lastElement = useRef();
 
-  /*
-  для того чтобы у нас был доступ k этому  observer внутри компонента,создадим еще один reference. On нужен не только для получения доступа к дом элементу, также в него можно сохранять какие-то данные чтобы не терять их от рендера к рендыру.
-  новый interceptionObserver мы помещаем в поле current этого reference. теперь нам необходимо указать за каким элементом мы будем наблюдать.
-   для этого вызываем у  observer функцию  observe() и туда необходимо передать дом элемент. нашем случае это lastElement. таким образом этот div теперь у нас является наблюдаемым и каждый раз когда div появляются в зоне видимости будет отрабатывать  этот callback
-  */
-  const observer = useRef();
-  useEffect(() => {
-    if (isPostLoading) return;
-    if (observer.current) observer.current.disconnect();
-    /* если observer уже создан(в поле current что-то находится),
-     то тогда мы должны отключить наблюдение со всеми элементами он за которыми наблюдает.
-     после чего мы можем смело создавать новый, с замыканием на ноом номере страницы.
-      сразу же необходимо сделать ограничением чтобы этот callback отрабатывал только тогда, когда номер текущей страницы меньше чем общее количество страниц
-     */
-    let callback = function (entries, observer) {
-      /* entries -массив элементов за которыми мы наблюдаем */
-      if (entries[0].isIntersecting && page < totalPages) {
-        /* увеличм номер стрницы, когда наблюдаемый див виден.  */
-        setPage(page + 1);
-      }
-    };
-    observer.current = new IntersectionObserver(callback);
-    observer.current.observe(lastElement.current);
-  }, [isPostLoading]);
+  useObserver(lastElement, page < totalPages, isPostLoading, () => {
+    setPage(page + 1);
+  });
 
   useEffect(() => {
     fetchPosts(limit, page);
-  }, [page]);
+  }, [page, limit]);
 
   const changePage = (page) => {
     setPage(page);
@@ -88,6 +68,17 @@ function Posts() {
         <PostForm create={createPost} />
       </MyModal>
       <PostFilter filter={filter} setFilter={setFilter} />
+      <MySelect
+        value={limit}
+        onChange={(value) => setLimit(value)}
+        defaultValue="Кол-во элементов на странице"
+        options={[
+          { value: 5, name: "5" },
+          { value: 10, name: "10" },
+          { value: 25, name: "25" },
+          { value: -1, name: "Показать все" },
+        ]}
+      />
       {postError && <h1>Произошла ошибка {postError} </h1>}
 
       <PostList posts={sortrdAndSearchedPosts} remove={removePost} title="Список постов" />
@@ -105,5 +96,30 @@ function Posts() {
 export default Posts;
 /*
 https://developer.mozilla.org/ru/docs/Web/API/Intersection_Observer_API
+  const observer = useRef();
+для того чтобы у нас был доступ k этому  observer внутри компонента,создадим еще один reference. On нужен не только для получения доступа к дом элементу, также в него можно сохранять какие-то данные чтобы не терять их от рендера к рендыру.
+  новый interceptionObserver мы помещаем в поле current этого reference. теперь нам необходимо указать за каким элементом мы будем наблюдать.
+   для этого вызываем у  observer функцию  observe() и туда необходимо передать дом элемент. нашем случае это lastElement. таким образом этот div теперь у нас является наблюдаемым и каждый раз когда div появляются в зоне видимости будет отрабатывать  этот callback
+
+
+useEffect(() => {
+    if (isPostLoading) return;
+    if (observer.current) observer.current.disconnect();
+    /* если observer уже создан(в поле current что-то находится),
+     то тогда мы должны отключить наблюдение со всеми элементами он за которыми наблюдает.
+     после чего мы можем смело создавать новый, с замыканием на ноом номере страницы.
+      сразу же необходимо сделать ограничением чтобы этот callback отрабатывал только тогда, когда номер текущей страницы меньше чем общее количество страниц
+
+    let callback = function (entries, observer) {
+       entries -массив элементов за которыми мы наблюдаем
+      if (entries[0].isIntersecting && page < totalPages) {
+         увеличм номер стрницы, когда наблюдаемый див виден.
+        setPage(page + 1);
+      }
+    };
+    observer.current = new IntersectionObserver(callback);
+    observer.current.observe(lastElement.current);
+  }, [isPostLoading]);
+
 
 */
